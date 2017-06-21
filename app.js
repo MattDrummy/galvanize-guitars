e459a6f2893d07025994ab5281954c5fe182a79b
Matthew Drumheller <MattDrummy@gmail.com>
2017-06-21 11:46:18 -0600

    All errors fixed

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-$(document).ready(function() {
   var $favorites = $('.favorites');
   var $selectedImage = $('.selected-img > img');
-  var $unfavorite = $('button');
+  var $unfavorite = $('.unfavorite');
   $favorites.click(changeImage);
   $unfavorite.click(unfavorite);
-})
+  //
+  function changeImage(event) {
+    switch (this.children[0].innerText) {
+      case 'Jackson - Electric':
+        $selectedImage.attr('src', 'assets/guitar1.jpg');
+        break;
+      case "Ibanoz - Electric":
+        $selectedImage.attr('src', 'assets/guitar2.jpg');
+        break;
+      case "Jackson - Orange Electric":
+        $selectedImage.attr('src', 'assets/guitar3.png');
+        break;
+      case "Grand Concert - Acoustic":
+        $selectedImage.attr('src', 'assets/guitar4.png');
+        break;
+      default:
 
-function changeImage(event) {
-  switch (this.children[0].innerText) {
-    case 'Jackson - Electric':
-      $selectedImage.attr('src', 'assets/guitar1.jpg');
-      break;
-    case "Ibanoz - Electric":
-      $selectedImage.attr('src', 'assets/guitar2.jpg');
-      break;
-    case "Jackson - Orange Electric":
-      $selectedImage.attr('src', 'assets/guitar3.png');
-      break;
-    case "Grand Concert - Acoustic":
-      $selectedImage.attr('src', 'assets/guitar4.png');
-      break;
-    default:
-
+    }
   }
-}
 
-function unfavorite(event) {
-  this.parentElement.parentElement.remove();
-}
+  function unfavorite(event) {
+    this.parentElement.parentElement.remove();
+  }
